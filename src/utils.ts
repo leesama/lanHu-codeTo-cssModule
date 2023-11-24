123ce1b29ebcc53eb96e850e93e6eef3d48768ef
leesher <784852885@qq.com>
2023-11-24 03:45:42 +0000

    ci: test webhook

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,7 @@
-// 判断字符串是不是html
+
 export function isHTML(str: string) {
   return /<[a-z][\s\S]*>/i.test(str);
 }
-// 判断字符串是css
 export function isCSS(str: string) {
   return /{[\s\S]*}/.test(str);
 }
